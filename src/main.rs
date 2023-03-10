1609b2c2a57c3610c20a555f7fc48062d2d8766f
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-03-10 08:47:48 +0530

    cleaning and not supporting floats

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,7 @@ impl ProgState {
     }
 }
 
-fn tokenize_line(s: &str, state: &mut ProgState) /* -> Vec<u8>  */{
-    // let v = Vec::new();
+fn tokenize_line(s: &str, state: &mut ProgState) {
     for c in s.bytes() {
         if c >= b'0' && c <= b'9' {
             if state.curr_num.is_none() {
@@ -71,6 +70,7 @@ fn tokenize_line(s: &str, state: &mut ProgState) /* -> Vec<u8>  */{
 
         if let Some(num) = state.curr_num {
             state.stack.push(num);
+            state.curr_num = None;
         }
 
         match c {
@@ -94,7 +94,7 @@ fn tokenize_line(s: &str, state: &mut ProgState) /* -> Vec<u8>  */{
 
             b'^' => state.two_operands_op(|a, b| Ok(a.pow(b as u32))),
 
-            b'.' => println!("change num to float"),
+            b'.' => eprintln!("floating point numbers not supported"),
 
             b'~' | b'_' => {
                 match state.stack.pop() {
@@ -109,17 +109,9 @@ fn tokenize_line(s: &str, state: &mut ProgState) /* -> Vec<u8>  */{
             _ => continue,
         }
     }
-    // v
 }
 
-// fn loop_over_input(r: &mut impl Read) {
-//     tokenize_line(s);
-// }
-
 fn main() {
-
-    // loop_over_input(&mut std::io::stdin().lock());
-
     let args: Vec<String> = std::env::args().collect();
     let mut line_buf: String;
     let mut state = ProgState::new(&args[0][..]);
@@ -130,9 +122,7 @@ fn main() {
             Ok(_) => tokenize_line(&line_buf[..], &mut state),
             Err(e) => {
                 println!("Err: {e}");
-                // Vec::new()
             }
         };
     }
-
 }
